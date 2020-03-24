77bebae9e461afd46b564ee2666abfde69abf9f5
erickogi <erickogi14@gmail.com>
2020-03-24 23:34:34 +0300

    Add Color asset and signup page

---
+++ LoginSample/UIViewController+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  UIViewController+Extensions.swift
+//  LoginSample
+//
+//  Created by Admin on 24/03/2020.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+extension UIViewController{
+    @IBAction func unwind(_segue: UIStoryboardSegue){}
+}

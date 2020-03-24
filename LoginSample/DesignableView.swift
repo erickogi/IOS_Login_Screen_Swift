bb425b6401afc2de4729f94fc342f98cef1ff33f
erickogi <erickogi14@gmail.com>
2020-03-24 21:53:32 +0300

    Initial Commit

---
+++ LoginSample/DesignableView.swift
@@ -0,0 +1,37 @@
+//
+//  DesignableView.swift
+//  LoginSample
+//
+//  Created by Admin on 24/03/2020.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable
+class DesignableView: UIView {
+    @IBInspectable var shadowColor: UIColor = UIColor.clear {
+        didSet {
+            layer.shadowColor = shadowColor.cgColor
+        }
+    }
+
+    @IBInspectable var shadowRadius: CGFloat = 0 {
+        didSet {
+            layer.shadowRadius = shadowRadius
+        }
+    }
+
+    @IBInspectable var shadowOpacity: CGFloat = 0 {
+        didSet {
+            layer.shadowOpacity = Float(shadowOpacity)
+        }
+    }
+
+    @IBInspectable var shadowOffsetY: CGFloat = 0 {
+        didSet {
+            layer.shadowOffset.height = shadowOffsetY
+        }
+    }
+}
---
+++ LoginSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginSample
+//
+//  Created by Admin on 24/03/2020.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
